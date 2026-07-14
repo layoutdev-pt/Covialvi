13341393f7fdd00176b7f392a13c9cca85b4f4f2
miguel olegna <miguel.olegna1@gmail.com>
2026-07-14 23:46:05 +0100

    mudar textos do foco

--- src/app/(public)/home-client.tsx
+++ src/app/(public)/home-client.tsx
@@ -440,8 +440,8 @@ export function HomeClient({ properties, featuredProperties, premiumHighlights =
             <>
               {!isSearchActive && premiumHighlights && premiumHighlights.length > 0 && (
                 <div className="flex items-center gap-2 mb-6">
-                  <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-foreground mb-6 tracking-tight leading-tight">
-                    OPORTUNIDADES ÚNICAS
+                  <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-foreground mb-6 mt-6 tracking-tight leading-tight">
+                    OPORTUNIDADES ÚNICAS <br />
                     <span className="text-muted-foreground">E IMPERDÍVEIS</span>
                   </h1>
                 </div>
